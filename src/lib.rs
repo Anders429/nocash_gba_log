048443bdd4b447cec5fa754a3e797ce32fc32dd1
Anders429 <anders.stefan.evensen@gmail.com>
2023-06-07 22:35:26 -0700

    Implement display traits for Error.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-#[no_std]
+#![no_std]
+
+use core::{fmt, fmt::Display};
 
 /// Location to check for the signature.
 const NOCASH_GBA_SIGNATURE_ADDRESS: *const [u8; 7] = 0x04FFFA00 as *const [u8; 7];
@@ -6,8 +8,17 @@ const NOCASH_GBA_SIGNATURE_ADDRESS: *const [u8; 7] = 0x04FFFA00 as *const [u8; 7
 /// This signature must be returned by the emulator for the logger to be enabled.
 const NOCASH_GBA_SIGNATURE: [u8; 7] = *b"no$gba ";
 
+#[derive(Debug)]
 pub enum Error {
     NotRunningInNoCashGba,
+}
+
+impl Display for Error {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::NotRunningInNoCashGba => fmt.write_str("not running in no$gba"),
+        }
+    }
 }
 
 pub fn init() -> Result<(), Error> {
