d93791c7bdc92172398039a1fe328f24316674fd
Anders429 <anders.stefan.evensen@gmail.com>
2023-06-07 22:31:46 -0700

    Verify whether program is running in no$gba.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
-pub fn init() -> Result<(), ()> {
-    todo!()
+#[no_std]
+
+/// Location to check for the signature.
+const NOCASH_GBA_SIGNATURE_ADDRESS: *const [u8; 7] = 0x04FFFA00 as *const [u8; 7];
+
+/// This signature must be returned by the emulator for the logger to be enabled.
+const NOCASH_GBA_SIGNATURE: [u8; 7] = *b"no$gba ";
+
+pub enum Error {
+    NotRunningInNoCashGba,
+}
+
+pub fn init() -> Result<(), Error> {
+    if unsafe { NOCASH_GBA_SIGNATURE_ADDRESS.read_volatile() } != NOCASH_GBA_SIGNATURE {
+        return Err(Error::NotRunningInNoCashGba);
+    }
+    todo!("initialize the logger")
 }
